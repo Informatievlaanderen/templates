d9be74d92e39a57cf496d42bc3a2f11ec27802d9
David Cumps <david@cumps.be>
2019-04-14 18:52:59 +0200

    feat: containerize projector

--- src/Be.Vlaanderen.Basisregisters.Templates/Be.Vlaanderen.Basisregisters.Registry/build.fsx
+++ src/Be.Vlaanderen.Basisregisters.Templates/Be.Vlaanderen.Basisregisters.Registry/build.fsx
@@ -77,6 +77,9 @@ Target "Pack_Solution" (fun _ ->
 Target "Containerize_Api" (fun _ -> containerize "ExampleRegistry.Api" "api")
 Target "PushContainer_Api" (fun _ -> push "api")
 
+Target "Containerize_Projector" (fun _ -> containerize "ExampleRegistry.Projector" "projector")
+Target "PushContainer_Projector" (fun _ -> push "projector")
+
 // --------------------------------------------------------------------------------
 
 Target "Build" DoNothing
@@ -86,28 +89,30 @@ Target "Pack" DoNothing
 Target "Containerize" DoNothing
 Target "Push" DoNothing
 
-"NpmInstall"         ==> "Build"
-"DotNetCli"          ==> "Build"
-"Clean"              ==> "Build"
-"Restore_Solution"   ==> "Build"
-"Build_Solution"     ==> "Build"
+"NpmInstall"              ==> "Build"
+"DotNetCli"               ==> "Build"
+"Clean"                   ==> "Build"
+"Restore_Solution"        ==> "Build"
+"Build_Solution"          ==> "Build"
 
-"Build"              ==> "Test"
-"Test_Solution"      ==> "Test"
+"Build"                   ==> "Test"
+"Test_Solution"           ==> "Test"
 
-"Test"               ==> "Publish"
-"Publish_Solution"   ==> "Publish"
+"Test"                    ==> "Publish"
+"Publish_Solution"        ==> "Publish"
 
-"Publish"            ==> "Pack"
-"Pack_Solution"      ==> "Pack"
+"Publish"                 ==> "Pack"
+"Pack_Solution"           ==> "Pack"
 
-"Pack"               ==> "Containerize"
-"Containerize_Api"   ==> "Containerize"
+"Pack"                    ==> "Containerize"
+"Containerize_Api"        ==> "Containerize"
+"Containerize_Projector"  ==> "Containerize"
 // Possibly add more projects to containerize here
 
-"Containerize"       ==> "Push"
-"DockerLogin"        ==> "Push"
-"PushContainer_Api"  ==> "Push"
+"Containerize"            ==> "Push"
+"DockerLogin"             ==> "Push"
+"PushContainer_Api"       ==> "Push"
+"PushContainer_Projector" ==> "Push"
 // Possibly add more projects to push here
 
 // By default we build & test
